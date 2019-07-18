61f0b0342b1d9065f227559166cc64435b430145
Miguel Angél Ramírez Escobar <miguel.ramirez@sofka.com.co>
2019-07-18 13:46:24 -0500

    reporte pdf

--- mvc.sql
+++ mvc.sql
@@ -4,67 +4,6 @@
 -- ------------------------------------------------------
 -- Server version	6.0.0-alpha-community-nt-debug
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `detallefactura`
---
-
-DROP TABLE IF EXISTS `detallefactura`;
-CREATE TABLE `detallefactura` (
-  `iddetalle` int(10) NOT NULL AUTO_INCREMENT,
-  `idfactura` int(10) NOT NULL,
-  `idproducto` int(10) NOT NULL,
-  `precio` int(10) NOT NULL,
-  `cantidad` int(8) NOT NULL,
-  `subtotal` int(10) NOT NULL,
-  PRIMARY KEY (`iddetalle`),
-  KEY `idproducto` (`idproducto`),
-  KEY `idfactura` (`idfactura`),
-  CONSTRAINT `detallefactura_ibfk_2` FOREIGN KEY (`idfactura`) REFERENCES `tblventa` (`idfactura`),
-  CONSTRAINT `detallefactura_ibfk_1` FOREIGN KEY (`idproducto`) REFERENCES `tblproductos` (`idproducto`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `detallefactura`
---
-
-LOCK TABLES `detallefactura` WRITE;
-/*!40000 ALTER TABLE `detallefactura` DISABLE KEYS */;
-/*!40000 ALTER TABLE `detallefactura` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tblclientes`
---
-
-DROP TABLE IF EXISTS `tblclientes`;
-CREATE TABLE `tblclientes` (
-  `cedula` int(10) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `telefono` varchar(30) NOT NULL,
-  `direccion` varchar(50) NOT NULL,
-  PRIMARY KEY (`cedula`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblclientes`
---
-
-LOCK TABLES `tblclientes` WRITE;
-/*!40000 ALTER TABLE `tblclientes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tblclientes` ENABLE KEYS */;
-UNLOCK TABLES;
-
 --
 -- Table structure for table `tblproductos`
 --
@@ -86,8 +25,8 @@ CREATE TABLE `tblproductos` (
 --
 
 LOCK TABLES `tblproductos` WRITE;
-/*!40000 ALTER TABLE `tblproductos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tblproductos` ENABLE KEYS */;
+ALTER TABLE `tblproductos` DISABLE KEYS;
+ALTER TABLE `tblproductos` ENABLE KEYS;
 UNLOCK TABLES;
 
 --
@@ -112,8 +51,8 @@ CREATE TABLE `tblproveedores` (
 --
 
 LOCK TABLES `tblproveedores` WRITE;
-/*!40000 ALTER TABLE `tblproveedores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tblproveedores` ENABLE KEYS */;
+ALTER TABLE `tblproveedores` DISABLE KEYS; 
+ALTER TABLE `tblproveedores` ENABLE KEYS ;
 UNLOCK TABLES;
 
 --
@@ -135,18 +74,5 @@ CREATE TABLE `tblventa` (
 -- Dumping data for table `tblventa`
 --
 
-LOCK TABLES `tblventa` WRITE;
-/*!40000 ALTER TABLE `tblventa` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tblventa` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2019-07-18  1:43:12
