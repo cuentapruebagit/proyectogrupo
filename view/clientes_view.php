1e053d3b7945aebe625c48d41b1ef1c750db51c5
KLEIDER <kleidermachado@gmail.com>
2019-07-18 19:46:43 -0500

    se crea vista de clientes

---
+++ view/clientes_view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>CLIENTES</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+		<div class="row" style="margin-top: 20px; margin-botton: 30px;">
+			<div style="width: 50%;
+						border: solid; border-color: blue;margin-top: 20px; display: block;margin: auto;">
+				<div class="col-md-12 col-xs-12" style="text-align: center;">
+					<h1 style="font-family: -webkit-body;">CLIENTES</h1>
+				</div>
+			</div>
+		</div>
+		<div class="row" >
+			<div class="col-md-4 col-xs-12" style="text-align: left" >
+				<button style="width: 100%; margin-top: 30px; margin-left: 20px;" type="button" class="btn btn-secondary">Ver Clientes</button>
+				<button style="width: 100%; margin: 20px;" type="button" class="btn btn-secondary">Agregar Nuevo Cliente</button>
+				<button style="width: 100%; margin: 20px;" type="button" class="btn btn-secondary">Eliminar Clientes</button>
+				<button style="width: 100%; margin: 20px;" type="button" class="btn btn-secondary">Editar info Cliente</button>
+			</div>
+		</div>
+	</body>
+</html>
