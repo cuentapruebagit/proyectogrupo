e93ac113ca6c5bab6981742b5c9c10bf5afb038b
Julian Vasquez Laverde <vasquez.j-3@hotmail.com>
2019-07-18 20:43:32 -0500

    se crea menu principal

--- index.php
+++ index.php
@@ -1,20 +1,42 @@
-<?php
-require_once 'bd/conexion.php';
-require_once 'controller/persona_controller.php';
+<!DOCTYPE html>
+<html lang="ES">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
 
-$controller = new persona_controller();
+        <title>Principal Main</title>
+    </head>
+    <div class="container">
+        <header><h1>Developer Inc.</h1></header>
+        <br>
+    </div>
+    <body>
+        <h1 id="Title">MENU PRINCIPAL</h1>
+        <br>
+        <br>
 
-
-if(!empty($_REQUEST['m'])){
-    $metodo = $_REQUEST['m'];
-    if(method_exists($controller,$metodo)){
-        $controller->$metodo();
-    }else{
-        $controller->index();
-    }
-}else{
-    $controller->index();
-}
-
-
-?>
+       
+<div class="col-xs-3 col-md-10 col-lg-12" id="btn">
+            <button id="btn1" type="button" class="btn btn-secondary" style="width: 30%">Proveedores</button>
+            <br>
+            <button  type="button" class="btn btn-secondary" style="width: 30%"> Clientes</button>
+            <br>
+            <button  type="button" class="btn btn-secondary" style="width: 30%">Productos</button>
+            <br>
+            <button  type="button" class="btn btn-secondary" style="width: 30%">Ventas</button>
+            <br>
+            <button  type="button" class="btn btn-secondary" style="width: 30%">Detalle Factura</button>
+    </div>
+        
+<br>
+<br>
+<br>
+<br>
+<div class="container col-xs-3 col-md-9 col-lg-12">
+ <footer><h5>Empresa creada sin fines de lucro NIT 10.162.24.7</h5></footer>
+ </div>
+    </body>
+</html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="css/style.css">
