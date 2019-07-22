d3e67284b79eb9ca51da839497059ca12c27461c
KLEIDER <cuentapruebagit@gmail.com>
2019-07-22 11:14:59 -0500

    Se restaura Archivo Principal

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once 'bd/conexion.php';
 require_once 'controller/persona_controller.php';
 $controller = new persona_controller();
+/*Conexion Inicial*/
 if(!empty($_REQUEST['m'])){
     $metodo = $_REQUEST['m'];
     if(method_exists($controller,$metodo)){
